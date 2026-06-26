69e06968cc8619d41eb23e4554f1f6e347393bb2
Лукянова Ю. А. <yu.lykyanova@e-u.edu.ua>
2026-06-26 18:06:52 +0200

    add users entity and migration

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,20 +3,26 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Category } from './categories/category.entity';
 import { Product } from './products/product.entity';
+import { User } from './users/user.entity';
 import { CreateTables1700000001000 } from './migrations/1700000001000-CreateTables';
 import { AddIsActiveToProducts1781921208651 } from './migrations/1781921208651-AddIsActiveToProducts';
+import { CreateUsers1782172648690 } from './migrations/1782172648690-CreateUsers';
 import { CacheModule } from '@nestjs/cache-manager';
 import { redisStore } from 'cache-manager-redis-yet';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CategoriesModule } from './categories/categories.module';
 import { ProductsModule } from './products/products.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     CategoriesModule,
     ProductsModule,
+    UsersModule,
+    AuthModule,
 
    TypeOrmModule.forRoot({
   type: 'postgres',
@@ -25,12 +31,13 @@ import { ProductsModule } from './products/products.module';
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
-  entities: [Category, Product],
+  entities: [Category, Product, User],
   synchronize: false,
   migrationsRun: true,
   migrations: [
   CreateTables1700000001000,
   AddIsActiveToProducts1781921208651,
+  CreateUsers1782172648690,
 ],
 }),
 
