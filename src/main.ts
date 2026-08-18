de34197c8c812b0e43198b78a646432c59a809ad
Лукянова Ю. А. <yu.lykyanova@e-u.edu.ua>
2026-08-18 20:05:33 +0200

    add interceptors and exception filter

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { TrimPipe } from './common/pipes/trim.pipe';
+import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
+import { TransformInterceptor } from './common/interceptors/transform.interceptor';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +19,25 @@ async function bootstrap() {
     }),
   );
 
+      app.useGlobalInterceptors(
+    new LoggingInterceptor(),
+    new TransformInterceptor(),
+  );
+
+  app.useGlobalFilters(new HttpExceptionFilter());
+  const config = new DocumentBuilder()
+    .setTitle('MiniShop API')
+    .setDescription(
+      'REST API для навчального інтернет-магазину. ' +
+        'Автентифікація через JWT Bearer token.',
+    )
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+  
   await app.listen(3000);
 }
 
